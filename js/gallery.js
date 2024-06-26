83420dcfffc6ff806ee6db4c8a5c20249577f53a
marek <97616874+xmakuwa@users.noreply.github.com>
2024-06-26 16:20:54 +0200

    js updates

--- js/gallery.js
+++ js/gallery.js
@@ -1,45 +1,43 @@
-
-
-import { galleryItems } from './gallery-items.js';
-
+import { galleryItems } from "./gallery-items.js";
 
 const ulGallery = document.querySelector(".gallery");
 
-const arrayDiv=galleryItems.map((item) => {
-    // // const ulImage=getElementsByClassName('gallery')
-    const divBoxImage = document.createElement("div");
-    divBoxImage.setAttribute('class', 'gallery__item');
-    const aImage = document.createElement('a');
-    aImage.setAttribute('class', 'gallery__link');
-    const image = document.createElement('img');
-    image.setAttribute('class', 'gallery__image');
-    image.src=item.preview;
-    image.alt=item.description;
-    image.dataset.source=item.original;
-    aImage.append(image);
-    divBoxImage.append(aImage);
-    return divBoxImage;
+const arrayDiv = galleryItems.map((item) => {
+	// // const ulImage=getElementsByClassName('gallery')
+	const divBoxImage = document.createElement("div");
+	divBoxImage.setAttribute("class", "gallery__item");
+	const aImage = document.createElement("a");
+	aImage.setAttribute("class", "gallery__link");
+	const image = document.createElement("img");
+	image.setAttribute("class", "gallery__image");
+	image.src = item.preview;
+	image.alt = item.description;
+	image.dataset.source = item.original;
+	aImage.append(image);
+	divBoxImage.append(aImage);
+	return divBoxImage;
 });
 ulGallery.append(...arrayDiv);
 
 ulGallery.addEventListener("click", (event) => {
-    event.preventDefault();
-    
-    const lightbox = basicLightbox.create(`
+	event.preventDefault();
+
+	const lightbox = basicLightbox.create(
+		`
     <img src="${event.target.dataset.source}" width="800" height="600">
-    `, {
-        onShow: (lightbox) => { document.addEventListener("keydown",keyEsc);  },
-    });
-    
-    lightbox.show();
+    `,
+		{
+			onShow: (lightbox) => {
+				document.addEventListener("keydown", keyEsc);
+			},
+		}
+	);
 
-    function keyEsc(event) {
-    if (event.key === 'Escape') {
-        lightbox.close();
-        }
-    }
+	lightbox.show();
+
+	function keyEsc(event) {
+		if (event.key === "Escape") {
+			lightbox.close();
+		}
+	}
 });
-
-
-
-
